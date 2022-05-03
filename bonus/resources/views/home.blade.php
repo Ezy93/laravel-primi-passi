6751173d1cc0e4f12351abc07678393dda3687ea
Ezy93 <giordani001@gmail.com>
2022-05-03 15:05:51 +0200

    creata cartella per bonus e copiato tutto lo scaffholding di laravel

---
+++ bonus/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel primi passi</title>
+</head>
+<body>
+    <main>
+        <ul>
+            @foreach ($stringhe as $String)
+                <li>{{$String}}</li>
+            @endforeach
+        </ul>
+    </main>
+</body>
+</html>
